edc79576ec88dccfadb7a44a160bcd6274782d07
AceOfKestrels <aceofkestrels@gmail.com>
2025-04-03 11:31:29 +0200

    #5 Add steam

---
+++ modules/gaming/steam.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+    };    
+    
+    environment.systemPackages = with pkgs; [
+        steam
+        protontricks
+    ];
+}

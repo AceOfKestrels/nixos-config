004993b932f7644903e29bfda0b4a71f0b1800eb
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-05-03 10:33:43 +0200

    added proton mail

---
+++ annika/software/standert.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        discord # Install Official Discord app
+        google-chrome # Install google chrom
+        plex # Install Plex
+        protonmail-desktop # Install Proton Mail
+    ];
+}

f4e10c8083f15f86f092b016423d2516fcc4e2ac
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-06-12 19:53:30 +0200

    fixed nfs drives

--- annika/drivers/nasdrive.nix
+++ annika/drivers/nasdrive.nix
@@ -1,39 +1,66 @@
 { config, pkgs, ... }:
+
 {
+  # Enable support for NFS filesystems at boot
   boot.supportedFilesystems = [ "nfs" ];
+
+  # RPC binder for NFS locking
   services.rpcbind.enable = true;
 
+  # Ensure NFS utilities are installed
+  environment.systemPackages = with pkgs; [ nfs-utils ];
+
+  # Define NFS mounts
   systemd.mounts = [
+    # Personal NAS share
     {
-      what        = "10.100.20.10:/mnt/bunker.nas.local.wa-ls.net/home-storage";
-      where       = "/home/annika/disks/nas-personal";
-      type        = "nfs";
-      mountConfig = {
-        Options = "rw,hard,intr,proto=tcp,_netdev";
-      };
+      what  = "10.100.20.10:/mnt/bunker.nas.local.wa-ls.net/home-storage";
+      where = "/home/annika/disks/nas-personal";
+      type  = "nfs4";
+      options = builtins.concatStringsSep "," [
+        "rw"
+        "hard"
+        "intr"
+        "proto=tcp"
+        "_netdev"
+        "exec"
+        "nolock"
+        "noatime"
+        "rsize=1048576"
+        "wsize=1048576"
+      ];
     }
+
+    # Games NAS share
     {
-      what        = "10.100.20.10:/mnt/bunker.nas.local.wa-ls.net/games";
-      where       = "/home/annika/disks/nas-games";
-      type        = "nfs";
-      mountConfig = {
-        Options = "rw,hard,intr,proto=tcp,_netdev";
-      };
+      what  = "10.100.20.10:/mnt/bunker.nas.local.wa-ls.net/games";
+      where = "/home/annika/disks/nas-games";
+      type  = "nfs4";
+      options = builtins.concatStringsSep "," [
+        "rw"
+        "hard"
+        "intr"
+        "proto=tcp"
+        "_netdev"
+        "exec"
+        "nolock"
+        "noatime"
+        "rsize=1048576"
+        "wsize=1048576"
+      ];
     }
   ];
 
+  # Automount units to unmount after idle
   systemd.automounts = [
     {
-      where           = "/home/annika/disks/nas-personal";
-      wantedBy        = [ "multi-user.target" ];
-      automountConfig = { TimeoutIdleSec = "10min"; };
+      where    = "/home/annika/disks/nas-personal";
+      wantedBy = [ "multi-user.target" ];
     }
+
     {
-      where           = "/home/annika/disks/nas-games";
-      wantedBy        = [ "multi-user.target" ];
-      automountConfig = { TimeoutIdleSec = "10min"; };
+      where    = "/home/annika/disks/nas-games";
+      wantedBy = [ "multi-user.target" ];
     }
   ];
-
-  environment.systemPackages = with pkgs; [ nfs-utils ];
 }

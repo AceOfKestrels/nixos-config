fb8129eba26d9c81d9fa9544ca0be58c714089c5
AceOfKestrels <aceofkestrels@gmail.com>
2025-06-15 19:02:54 +0200

    [lexi] ignore ownership of config directory

--- lexi/scripts/update-system.sh
+++ lexi/scripts/update-system.sh
@@ -2,6 +2,7 @@
 
 sleep 60
 cd "$NIXOS_CONFIG_DIR"
+git config --global safe.directory "$NIXOS_CONFIG_DIR"
 git fetch --prune
 git reset --hard "origin/$NIXOS_CONFIG_REMOTE_BRANCH"
 nix-channel --add "$NIXOS_CONFIG_CHANNEL" nixos 

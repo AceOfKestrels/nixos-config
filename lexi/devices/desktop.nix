ffaad608890e90d923f604f5e9870bfefaf7b090
AceOfKestrels <aceofkestrels@gmail.com>
2025-06-15 14:19:05 +0200

    Add lexi config

---
+++ lexi/devices/desktop.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+    imports = [
+        ../modules/amdgpu.nix
+
+        ../../modules/desktop/kde-plasma.nix
+
+        ../../modules/core/office.nix
+        ../../modules/core/garbage-collect.nix
+
+        ../../modules/gaming/minecraft.nix
+        ../../modules/gaming/steam.nix
+    ];
+
+    programs.git.enable = true;
+
+    security.sudo.wheelNeedsPassword = false;
+
+    environment.systemPackages = with pkgs; [
+        firefox
+        firefox-devedition
+
+        discord
+        vesktop
+
+        neovim
+
+        fastfetch
+        hyfetch
+    ];
+}
---
+++ lexi/modules/amdgpu.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+    # Not strictly necessary, since it's installed implicitly by setting the video drivers
+    boot.initrd.kernelModules = [ "amdgpu" ];
+
+    services.xserver.enable = true;
+    services.xserver.videoDrivers = [ "amdgpu" ];
+
+    hardware.graphics = {
+        extraPackages = with pkgs; [
+            vulkan-loader
+            amdvlk
+        ];
+
+        # Same for 32 bit
+        enable32Bit = true;
+        extraPackages32 = with pkgs.pkgsi686Linux; [
+            vulkan-loader
+            amdvlk
+        ];
+    };
+
+    environment.systemPackages = with pkgs; [
+        vulkan-tools
+        glxinfo
+        linux-firmware
+    ];
+
+    environment.variables.VK_LOADER_DRIVERS_DISABLE = "*dzn*";
+}

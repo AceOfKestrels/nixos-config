2769496a0afe6cf0d4b90c36b95848f62a96f42c
AceOfKestrels <aceofkestrels@gmail.com>
2025-06-15 19:25:27 +0200

    [lexi] add sudo to script

--- lexi/scripts/update-system.sh
+++ lexi/scripts/update-system.sh
@@ -5,5 +5,5 @@ cd "$NIXOS_CONFIG_DIR"
 git config --global safe.directory "$NIXOS_CONFIG_DIR"
 git fetch --prune
 git reset --hard "origin/$NIXOS_CONFIG_REMOTE_BRANCH"
-nix-channel --add "$NIXOS_CONFIG_CHANNEL" nixos 
-nixos-rebuild switch --upgrade
+sudo nix-channel --add "$NIXOS_CONFIG_CHANNEL" nixos 
+sudo nixos-rebuild switch --upgrade

027695e1ba24813cdbde2238630059dbe4e13e3d
AceOfKestrels <aceofkestrels@gmail.com>
2025-07-01 21:56:43 +0200

    Updated garbage collection

--- modules/core/garbage-collect.nix
+++ modules/core/garbage-collect.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 
 {
+    # collect garbage automatically, every week
     nix.gc.automatic = true;
-    nix.gc.dates = "weekly"; # or daily/hourly etc.
+    nix.gc.dates = "weekly";
+
+    # deduplicate store files
+    nix.settings.auto-optimise-store = true;
+
+    # keep store blobs for old generations up to 30 days
     nix.gc.options = "--delete-older-than 30d";
+
+    # only keep the last five generations (otherwise boot partition can fill up too much)
+    boot.loader.systemd-boot.configurationLimit = 5;
 }

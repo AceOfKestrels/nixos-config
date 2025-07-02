f14e4df41b483fc11d8d06c85804144922bf5161
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-07-02 11:50:03 +0200

    [Global] Fixed Freelans PKG

--- modules/development/terraform.nix
+++ modules/development/terraform.nix
@@ -1,33 +1,49 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      freelens = super.stdenv.mkDerivation {
-        pname = "freelens";
-        version = "1.4.0";
-        src = super.fetchurl {
-          url = "https://github.com/freelensapp/freelens/releases/download/v1.4.0/Freelens-1.4.0-linux-amd64.AppImage";
-          sha256 = "178rdvzycs23avlhg27p579llc0rcmygmy2pg98hmdn6lp1gdx4n";
-        };
-        nativeBuildInputs = [ super.makeWrapper super.fuse ];
-        unpackPhase = "true";
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $src $out/bin/freelens.AppImage
-          chmod +x $out/bin/freelens.AppImage
-          makeWrapper $out/bin/freelens.AppImage $out/bin/freelens
-        '';
-      };
-    })
-  ];
+    nixpkgs.overlays = [
+        (self: super: {
+        freelens = super.stdenv.mkDerivation rec {
+            pname = "freelens";
+            version = "1.4.0";
 
-  environment.systemPackages = with pkgs; [
-    terraform
-    packer
-    kubectl
-    hcloud
-    coreutils
-    freelens
-  ];
+            src = super.fetchurl {
+                url = "https://github.com/freelensapp/freelens/releases/download/v1.4.0/Freelens-1.4.0-linux-amd64.deb";
+                sha256 = "1gg1cm34rzll5pdv5v6fqgg9k6lkh9vhfg9mikd7glmifzvsrrp3";
+            };
+
+            nativeBuildInputs = [ super.dpkg super.makeWrapper ];
+
+            unpackPhase = ''
+                dpkg-deb -x $src .
+            '';
+
+            installPhase = ''
+                mkdir -p $out/bin
+                install -Dm755 ./opt/Freelens/freelens $out/bin/freelens
+
+                # Install .desktop file
+                mkdir -p $out/share/applications
+                install -Dm644 ./usr/share/applications/freelens.desktop $out/share/applications/freelens.desktop
+
+                # Install icons
+                mkdir -p $out/share/icons
+                cp -r ./usr/share/icons/hicolor $out/share/icons/
+
+                # Wrap binary if needed
+                wrapProgram $out/bin/freelens \
+                    --prefix PATH : ${super.lib.makeBinPath [ super.coreutils ]}
+            '';
+        };
+        })
+    ];
+
+    environment.systemPackages = with pkgs; [
+        terraform
+        packer
+        kubectl
+        hcloud
+        coreutils
+        freelens
+    ];
 }

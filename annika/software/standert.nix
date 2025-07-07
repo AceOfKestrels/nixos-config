2d6438c54ceb4334015233cd78b5390d2138a1cd
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-07-07 19:53:48 +0200

    [Annika] fixed plex

--- annika/software/standert.nix
+++ annika/software/standert.nix
@@ -11,7 +11,7 @@
     environment.systemPackages = with pkgs; [
         discord # Install Official Discord app
         google-chrome # Install google chrom
-        plex # Install Plex
+        plex-desktop # Install Plex
         protonmail-desktop # Install Proton Mail
         termius # SSH Client
     ];

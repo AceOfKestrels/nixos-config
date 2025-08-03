4372ebea99a9948c2081e85935c87209761dcfd2
AceOfKestrels <aceofkestrels@gmail.com>
2025-08-03 21:05:57 +0200

    [global] use home manager from channel

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,12 +1,7 @@
 { ... }:
 
-let
-    home-manager-src = builtins.fetchTarball {
-        url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-    };
-in
 {
-    imports = [ (import "${home-manager-src}/nixos") ];
+    imports = [ <home-manager/nixos> ];
 
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;

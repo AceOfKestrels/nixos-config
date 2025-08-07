e1faaa56286c204f46d40bbd631488e08730e597
AceOfKestrels <aceofkestrels@gmail.com>
2025-08-07 21:37:41 +0200

    [annika] add flake

---
+++ annika/devices/main-term/flake.nix
@@ -0,0 +1,42 @@
+{
+    description = "flake for annika-main-term. use 'sudo nixos-rebuild switch --flake /etc/nixos/annika/devices/main-term#main-term' to apply.";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+    outputs =
+        inputs@{
+            self,
+            nixpkgs,
+            home-manager,
+            ...
+        }:
+        let
+            hostname = "main-term";
+        in
+        {
+            nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = {
+                    inherit
+                        inputs
+                        ;
+                };
+                modules = [
+                    ./device.nix
+                    {
+                        environment.variables.FLAKE_PATH = "/etc/nixos/nixos-config/annika/devices/main-term";
+                        networking.hostName = nixpkgs.lib.mkForce hostname;
+                        nix.settings.experimental-features = [
+                            "nix-command"
+                            "flakes"
+                        ];
+                    }
+                ];
+            };
+        };
+}

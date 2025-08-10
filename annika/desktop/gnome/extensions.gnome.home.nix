28e9195c97734c56fa9355fa7c7b336391770f6a
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-08-10 21:52:48 +0200

    [Annika] completet moving dconf to nix conf

--- annika/desktop/gnome/extensions.gnome.home.nix
+++ annika/desktop/gnome/extensions.gnome.home.nix
@@ -23,5 +23,26 @@
                 "transparent-window-moving@noobsai.github.com"
             ];
         };
+
+        "org/gnome/shell/extensions/dash-to-dock" = {
+            background-opacity = 0.8;
+            dash-max-icon-size = 32;
+            dock-position = "BOTTOM";
+            height-fraction = 0.9;
+            multi-monitor = true;
+            preferred-monitor = -2;
+            preferred-monitor-by-connector = "DP-0";
+            preview-size-scale = 0.0;
+            show-mounts = false;
+        };
+
+        "org/gnome/shell/extensions/trayIconsReloaded" = {
+            icon-size = 16;
+            icons-limit = 8;
+        };
+
+        "org/gnome/tweaks" = {
+            show-extensions-notice = false;
+        };
     };
 }

aa5c137c2c4febcc3261bea6213dd3abd235977f
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-08-14 18:37:19 +0200

    [Global] Fixed Fit Cred Helper

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -1,8 +1,28 @@
 { pkgs, ... }:
-
+let
+    gcmWrapped = pkgs.symlinkJoin {
+        name = "git-credential-manager-wrapped";
+        paths = [ pkgs.git-credential-manager ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+            wrapProgram $out/bin/git-credential-manager \
+              --set LD_LIBRARY_PATH ${
+                  pkgs.lib.makeLibraryPath [
+                      pkgs.libsecret
+                      pkgs.glib
+                      pkgs.curl
+                      pkgs.krb5
+                      pkgs.openssl
+                  ]
+              } \
+              --set GCM_CREDENTIAL_STORE secretservice
+        '';
+    };
+in
 {
     programs.git = {
         enable = true;
+        package = pkgs.gitFull;
 
         config = {
             user = {
@@ -10,10 +30,9 @@
                 email = "changeme@example.com";
             };
             core.editor = "nano";
-            alias = {
-                pfusch = "push --force";
-            };
+            alias.pfusch = "push --force";
             init.defaultBranch = "main";
+
             credential.helper = "manager";
             credential.credentialStore = "secretservice";
             safe.directory = "/etc/nixos/nixos-config";
@@ -21,6 +40,9 @@
     };
 
     environment.systemPackages = with pkgs; [
-        git-credential-manager
+        gcmWrapped
+        xdg-utils # xdg-open so GCM can launch your browser
+        libsecret # not strictly required (wrapped), but handy for tools like secret-tool
+        seahorse # optional GUI to view saved creds
     ];
 }

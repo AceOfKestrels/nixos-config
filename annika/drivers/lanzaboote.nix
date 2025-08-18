ebd5d36eca1bda9744a83d304ba2dab635cadd18
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-08-18 23:39:29 +0200

    [Annika] moved systemd boot settings

--- annika/drivers/lanzaboote.nix
+++ annika/drivers/lanzaboote.nix
@@ -11,6 +11,20 @@
     # (Optional but common if you want TPM2 unlock later)
     boot.initrd.systemd.enable = true;
 
+    # Menu behavior
+    boot.loader.timeout = 5; # seconds
+    boot.loader.systemd-boot.editor = false;
+    boot.loader.systemd-boot.consoleMode = "max"; # or "keep" | "0".."5"
+
+    # Extra entries appear in /boot/loader/entries/*.conf
+    boot.loader.systemd-boot.extraEntries = {
+        "windows.conf" = ''
+            title   Windows Fuckery
+            sort-key 20-windows
+            efi     /EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+    };
+
     environment.systemPackages = with pkgs; [
         sbctl
     ];

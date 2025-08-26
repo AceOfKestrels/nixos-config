03baa76b7d03810f4d72109268557df69466179a
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-08-26 20:01:56 +0200

    [Global] pined terminus

--- overlays/core.overlay.nix
+++ overlays/core.overlay.nix
@@ -1,9 +1,7 @@
 { ... }:
 
-# This module is used to pin the version of packages to a specific nixpkgs revision
 let
-    # Set this to the nixpkgs commit hash and the archive hash you want to use
-    # You can use the alias "prefetch-current-build" below to fetch them
+    # Pin info
     nixpkgs-revision = "9807714d6944a957c2e036f84b0ff8caf9930bc0";
     nixpkgs-sha256 = "1g9qc3n5zx16h129dqs5ixfrsff0dsws9lixfja94r208fq9219g";
 
@@ -11,22 +9,31 @@ let
         url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgs-revision}.tar.gz";
         sha256 = nixpkgs-sha256;
     };
-    pinnedPkgs = import pinnedSrc {
-        system = builtins.currentSystem;
-        config = {
-            allowUnfree = true;
-        };
-    };
 in
 {
     nixpkgs.overlays = [
-        (self: super: {
-            # Add packages to override like
-            foobar = pinnedPkgs.foobar;
-        })
+        (
+            final: prev:
+            let
+                pinnedPkgs = import pinnedSrc {
+                    system = prev.stdenv.hostPlatform.system; # ← flake-safe source of system
+                    config = {
+                        allowUnfree = true;
+                    };
+                };
+            in
+            {
+                # Use packages from the pinned nixpkgs
+                foobar = pinnedPkgs.foobar;
+                termius = pinnedPkgs.termius;
+            }
+        )
     ];
 
     environment.shellAliases = {
-        prefetch-current-build = ''rev=$(nixos-version --revision); echo "revision: $rev"; sha=$(nix-prefetch-url --unpack "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" --print-path); echo "sha256: $sha" | head -n1'';
+        prefetch-current-build = ''
+            rev=$(nixos-version --revision); echo "revision: $rev";
+                    sha=$(nix-prefetch-url --unpack "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" --print-path);
+                    echo "sha256: $sha" | head -n1'';
     };
 }

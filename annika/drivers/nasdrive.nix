d3eff7f56e3a146ad360a7a9f094f236b02d702d
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-09-10 18:44:26 +0200

    [annika] added media drive

--- annika/drivers/nasdrive.nix
+++ annika/drivers/nasdrive.nix
@@ -49,6 +49,25 @@
                 "wsize=1048576"
             ];
         }
+
+        # Media NAS share
+        {
+            what = "10.100.20.11:/volume1/media";
+            where = "/home/annika/disks/media";
+            type = "nfs4";
+            options = builtins.concatStringsSep "," [
+                "rw"
+                "hard"
+                "intr"
+                "proto=tcp"
+                "_netdev"
+                "exec"
+                "nolock"
+                "noatime"
+                "rsize=1048576"
+                "wsize=1048576"
+            ];
+        }
     ];
 
     # Automount units to unmount after idle

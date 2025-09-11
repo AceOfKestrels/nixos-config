c4cc70367026cc45aa1f6e406d268c6361f2b827
AceOfKestrels <aceofkestrels@gmail.com>
2025-09-11 18:45:08 +0200

    add overlay list func

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -1,9 +1,10 @@
-{ system, ... }:
+{ system, lib, ... }:
 
 rec {
-    mkPinned = package: revision: mkPinnedFromSource package revision "github:NixOS/nixpkgs";
-
-    mkPinnedFromSource =
+    # package => revision => source => ( final => prev => {} )
+    # creates an overlay that pins <package> from <source> to <revision>
+    # example: mkOverlayFromSource "ytmdesktop" "d179d77c139e0a3f5c416477f7747e9d6b7ec315" "github:NixOS/nixpkgs"
+    mkOverlayFromSource =
         package: revision: source:
         (
             final: prev:
@@ -11,13 +12,22 @@ rec {
                 src = builtins.getFlake "${source}?rev=${revision}";
                 pinned = import src {
                     inherit system;
-                    config = {
-                        allowUnfree = true;
-                    };
+                    overlays = [ ];
+                    config = prev.config or { allowUnfree = true; };
                 };
             in
             {
                 ${package} = pinned.${package};
             }
         );
+
+    # package => revision => (final => prev => {})
+    # creates an overlay that pins <package> from NixOS/nixpkgs to <revision>
+    # example: mkOverlay "ytmdesktop" "d179d77c139e0a3f5c416477f7747e9d6b7ec315"
+    mkOverlay = package: revision: mkOverlayFromSource package revision "github:NixOS/nixpkgs";
+
+    # args@{package=revision} => [(final => prev => {})]
+    # creates a list of overlays that pins each package from <args> to a specific revision
+    # example: mkOverlays { ytmdesktop = "d179d77c139e0a3f5c416477f7747e9d6b7ec315"; }
+    mkOverlays = args@{ ... }: lib.mapAttrsToList mkOverlay args;
 }

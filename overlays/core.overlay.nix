8f8dbbbd9c102b9c2f5a4e11d8c39530e6d67043
AceOfKestrels <aceofkestrels@gmail.com>
2025-09-11 18:45:08 +0200

    add overlay list func

--- overlays/core.overlay.nix
+++ overlays/core.overlay.nix
@@ -1,8 +1,9 @@
 { kestrel, ... }:
 
 {
-    nixpkgs.overlays = [
-        (kestrel.overlays.mkPinned "ytmdesktop" "d179d77c139e0a3f5c416477f7747e9d6b7ec315")
-        (kestrel.overlays.mkPinned "termius" "9807714d6944a957c2e036f84b0ff8caf9930bc0")
-    ];
+    nixpkgs.overlays = kestrel.overlays.mkOverlays {
+        ytmdesktop = "d179d77c139e0a3f5c416477f7747e9d6b7ec315";
+        termius = "9807714d6944a957c2e036f84b0ff8caf9930bc0";
+        # kdePackages.kate = "9807714d6944a957c2e036f84b0ff8caf9930bc0";
+    };
 }

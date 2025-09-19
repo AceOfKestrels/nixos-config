828892707438ba153bdec12700ea00e43e850d77
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2025-09-19 22:23:55 +0200

    [global] fix terminus

--- modules/terminal/terminus.nix
+++ modules/terminal/terminus.nix
@@ -4,11 +4,13 @@
     ...
 }:
 {
+    nixpkgs.overlays = kestrel.overlays.mkOverlays {
+        termius = "9807714d6944a957c2e036f84b0ff8caf9930bc0";
+    };
+}
+// {
 
     environment.systemPackages = with pkgs; [
         termius # SSH Client
     ];
-    nixpkgs.overlays = kestrel.overlays.mkOverlays {
-        termius = "9807714d6944a957c2e036f84b0ff8caf9930bc0";
-    };
 }

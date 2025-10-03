965171769ce3ca3e0be78343ba68e54fabefcf88
AceOfKestrels <aceofkestrels@gmail.com>
2025-10-03 18:40:21 +0200

    add configuration limit to grub

--- modules/bootloader/grub.nix
+++ modules/bootloader/grub.nix
@@ -3,7 +3,9 @@
     boot.loader.systemd-boot.enable = lib.mkForce false;
     boot.loader.grub = {
         enable = lib.mkForce true;
+        copyKernels = lib.mkForce true;
         efiSupport = true;
         device = "nodev";
+        configurationLimit = 5;
     };
 }

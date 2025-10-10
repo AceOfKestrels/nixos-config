1d6a4c355b4abbc98ea79c6e4fa73d6212594613
AceOfKestrels <aceofkestrels@gmail.com>
2025-10-10 08:19:58 +0200

    Pin all jetbrains packages

--- modules/development/dotnet.nix
+++ modules/development/dotnet.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ kestrel, pkgs, ... }:
 
 {
     environment.systemPackages = with pkgs; [
@@ -7,4 +7,8 @@
         dotnetCorePackages.dotnet_8.sdk # .NET 8
         dotnetCorePackages.dotnet_9.sdk # .NET 9
     ];
+
+    nixpkgs.overlays = kestrel.overlays.mkOverlays {
+        jetbrains = "8eaee110344796db060382e15d3af0a9fc396e0e";
+    };
 }

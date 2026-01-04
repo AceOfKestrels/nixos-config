786c19e6a81a5e3b98205e24be4e90b19182c073
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2026-01-04 18:28:13 +0100

    [Annika] adjustet audi

--- modules/utility/audio.annika.nix
+++ modules/utility/audio.annika.nix
@@ -6,13 +6,14 @@
     boot.kernelParams = [ "threadirqs" ];
 
     services.pipewire = {
+        enable = true;
         extraConfig.pipewire."92-low-latency" = {
             "context.properties" = {
                 "default.clock.rate" = 192000;
                 "default.clock.allowed-rates" = [ 192000 ];
-                "default.clock.quantum" = 64;
+                "default.clock.quantum" = 128;
                 "default.clock.min-quantum" = 32;
-                "default.clock.max-quantum" = 512;
+                "default.clock.max-quantum" = 128;
             };
         };
     };

0d7ecb08ae93329b9d1f1d22ff05b1a256f761ef
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2026-01-19 10:16:23 +0100

    [annika] Addedtidal and mailbox to fav in task bar

--- modules/desktop/annika/gnome/apps.gnome.nix
+++ modules/desktop/annika/gnome/apps.gnome.nix
@@ -36,15 +36,14 @@
             favorite-apps = [
                 "google-chrome.desktop"
                 "org.keepassxc.KeePassXC.desktop"
+                "tidal-hifi.desktop"
                 "org.gnome.Console.desktop"
                 "termius-app.desktop"
                 "org.gnome.Nautilus.desktop"
                 "steam.desktop"
-                "proton-mail.desktop"
+                "chrome-bancgfmehdjbgbdegmkbpkedapiclkma-Default.desktop"
                 "discord.desktop"
                 "signal.desktop"
-                "teamspeak.desktop"
-                "jellyflix.desktop"
             ];
 
         };

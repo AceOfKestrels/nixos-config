13eb3221fc47f381d0634fc9cfe0f995fa4e52e0
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2026-02-02 19:57:11 +0100

    [Annika] Set to not requier auth for mount an unount

--- modules/core.annika.nix
+++ modules/core.annika.nix
@@ -47,6 +47,21 @@
         ./utility/lix.nix
     ];
 
+    security.polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if ((action.id == "org.freedesktop.udisks2.filesystem-mount" ||
+               action.id == "org.freedesktop.udisks2.filesystem-mount-system" ||
+               action.id == "org.freedesktop.udisks2.filesystem-unmount-others" ||
+               action.id == "org.freedesktop.udisks2.encrypted-unlock" ||
+               action.id == "org.freedesktop.udisks2.eject-media" ||
+               action.id == "org.freedesktop.udisks2.drive-eject" ||
+               action.id == "org.freedesktop.udisks2.modify-device") &&
+              subject.isInGroup("wheel")) {
+            return polkit.Result.YES;
+          }
+        });
+    '';
+
     environment.systemPackages = with pkgs; [
         discord # Install Official Discord app
         google-chrome # Install google chrom

5aca1546802d30375b7798600e5cb83ffa0ca36c
AceOfKestrels <git@kestrels.dev>
2026-03-02 19:50:52 +0100

    allow unfree packages for nix shell

--- modules/core.nix
+++ modules/core.nix
@@ -61,6 +61,7 @@
     environment.variables = {
         NIXOS_CONFIG_PATH = "/etc/nixos/nixos-config";
         COMMIT_FLAKE_LOCK = 1;
+        NIXPKGS_ALLOW_UNFREE = 1;
     };
 
     environment.shellAliases = {

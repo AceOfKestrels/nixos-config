65bc3159bdc1e4cb79139d726930624d8d5e49a1
AceOfKestrels <git@kestrels.dev>
2026-04-03 03:24:57 +0200

    move device independent modules up

--- definitions/tags.pc.nix
+++ definitions/tags.pc.nix
@@ -2,24 +2,11 @@
 
 let
     sharedHmModules = [
-        ../modules/gaming/minecraft.home.nix
-        ../modules/development/dotnet.home.nix
-        ../modules/development/webdev.home.nix
-        ../modules/utility/jellyfin.home.nix
-        ../modules/utility/tidal.home.nix
-        ../modules/utility/discord.home.nix
     ];
     sharedSystemModules = [
-        ../modules/gaming/steam.nix
-        ../modules/utility/printing.nix
-        ../modules/utility/tor.nix
-        ../modules/utility/locale-en.nix
-        ../modules/utility/catppuccin.nix
     ];
 
     annikaHmModules = [
-        ../modules/utility/termius.home.nix
-        ../modules/utility/chrome.home.nix
         ../modules/development/php.home.nix
     ];
     annikaSystemModules = [
@@ -27,12 +14,9 @@ let
     ];
 
     kesHmModules = [
-        ../modules/utility/librewolf.home.nix
-        ../modules/utility/nvim.home.nix
-        ../modules/utility/drawing.home.nix
-        ../modules/utility/env.kes.home.nix
     ];
-    kesSystemModules = [ ];
+    kesSystemModules = [
+    ];
 
     melHmModules = [ ];
     melSystemModules = [ ];
--- modules/utility/librewolf.home.nix
+++ modules/utility/librewolf.home.nix
@@ -6,30 +6,5 @@
     programs.firefox = {
         enable = true;
         package = pkgs.librewolf;
-
-        profiles.default.settings = {
-            # enable firefox sync
-            "identity.fxaccounts.enabled" = true;
-
-            "privacy.clearOnShutdown.cache" = false;
-            "privacy.clearOnShutdown.downloads" = false;
-            "privacy.clearOnShutdown.history" = false;
-            "privacy.clearOnShutdown.cookies" = false;
-            "privacy.clearOnShutdown.formdata" = true;
-
-            "services.sync.prefs.sync.privacy.clearOnShutdown.cache" = false;
-            "services.sync.prefs.sync.privacy.clearOnShutdown.downloads" = false;
-            "services.sync.prefs.sync.privacy.clearOnShutdown.history" = false;
-            "services.sync.prefs.sync.privacy.clearOnShutdown.cookies" = false;
-            "services.sync.prefs.sync.privacy.clearOnShutdown.formdata" = true;
-
-            "browser.sessionstore.resume_from_crash" = true;
-            "browser.sessionstore.resuming_after_os_restart" = true;
-            "browser.sessionstore.resume_session_once" = true;
-
-            "privacy.resistFingerprinting" = false;
-            "privacy.fingerprintingProtection" = false;
-            "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme";
-        };
     };
 }

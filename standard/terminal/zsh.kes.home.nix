37a69b74f176dcc6c461ab3cb2f032d16167c8b8
AceOfKestrels <git@kestrels.dev>
2026-06-07 18:42:07 +0200

    [kes] add fetch and upgrade reminder

--- standard/terminal/zsh.kes.home.nix
+++ standard/terminal/zsh.kes.home.nix
@@ -28,5 +28,15 @@ in
         }
 
         ${p10k-prompt}
+
+        # rainbow fetch
+        fastfetch --logo-color-1 #f38ba8 --logo-color-2 #fab387 --logo-color-3 #f9e2af --logo-color-4 #a6e3a1 --logo-color-5 #89b4fa --logo-color-6 #cba6f7
+
+        # upgrade reminder
+        if [ -z "$(git -C "$FLAKE_PATH" log --since=$(date -d "-1day" +%s) "$FLAKE_PATH/flake.lock" )" ]; then
+            echo
+            echo "System was last upgraded $(git -C "$FLAKE_PATH" log --pretty=format:%ad --date=relative -n 1 $FLAKE_PATH/flake.lock)"
+            echo "Use 'upgrade' to run the NixOS system upgrade."
+        fi
     '';
 }

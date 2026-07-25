65224cecc530beec8dff47cc3a531b39f25bca19
Annika Leonie Keggenhoff <mail@annika-keggenhoff.de>
2026-07-25 10:15:31 +0200

    fixed format

--- standard/tools/vscode.nix
+++ standard/tools/vscode.nix
@@ -1,40 +1,40 @@
 {
-  kestrix,
-  pkgs,
-  ...
+    kestrix,
+    pkgs,
+    ...
 }:
 
 kestrix.mkHome {
-  programs.vscode = {
-    enable = true;
+    programs.vscode = {
+        enable = true;
 
-    # HM only adds new extensions to allow manual installation
-    # If you remove any here they need to be uninstalled manually
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      timonwong.shellcheck
-      github.vscode-github-actions
-      christian-kohler.path-intellisense
-      davidanson.vscode-markdownlint
-      ms-azuretools.vscode-docker
-      ms-vsliveshare.vsliveshare
-    ];
+        # HM only adds new extensions to allow manual installation
+        # If you remove any here they need to be uninstalled manually
+        profiles.default.extensions = with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            timonwong.shellcheck
+            github.vscode-github-actions
+            christian-kohler.path-intellisense
+            davidanson.vscode-markdownlint
+            ms-azuretools.vscode-docker
+            ms-vsliveshare.vsliveshare
+        ];
 
-    profiles.default.userSettings = {
-      "terminal.integrated.fontFamily" = "MesloLGS Nerd Font";
+        profiles.default.userSettings = {
+            "terminal.integrated.fontFamily" = "MesloLGS Nerd Font";
+        };
     };
-  };
 
-  fonts.fontconfig.enable = true;
-  home.packages = with pkgs; [
-    nerd-fonts.meslo-lg
-    nixfmt
-    nixd
-  ];
+    fonts.fontconfig.enable = true;
+    home.packages = with pkgs; [
+        nerd-fonts.meslo-lg
+        nixfmt
+        nixd
+    ];
 }
 // {
-  imports = kestrix.tagged {
-    kes = [ ./vscode.kes.home.nix ];
-    annika = [ ./vscode.annika.home.nix ];
-  };
+    imports = kestrix.tagged {
+        kes = [ ./vscode.kes.home.nix ];
+        annika = [ ./vscode.annika.home.nix ];
+    };
 }
